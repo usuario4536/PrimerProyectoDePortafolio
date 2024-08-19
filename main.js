65ba9a8465fdc0f4f16d09af18e45ff170688382
hiddenID <hiddenid342@gmail.com>
2024-08-19 13:45:11 -0500

    error

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 alert("hello world!!!!")
 let unPullDePrueba = prompt("Esto sera enviado al local :)")
 console.log(unPullDePrueba)
+
+let body = document.getElementById("presentacion")
+let boton = document.getElementById("boton")
+
+boton.addEventListener("click", funcion)
+
+function funcion(){
+    return document.getElementById("presentacion").style.display = "none";
+}
+
