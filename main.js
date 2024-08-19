fc3584fee81c9a54ca9b57be4abca4e81cf4edef
usuario4536 <163483128+usuario4536@users.noreply.github.com>
2024-08-19 14:07:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ console.log(unPullDePrueba)
 
 let body = document.getElementById("presentacion")
 let boton = document.getElementById("boton")
+let boton2 = document.getElementById("boton2")
 
 boton.addEventListener("click", funcion)
+boton2.addEventListener("click", funcion2)
 
 function funcion(){
     return document.getElementById("presentacion").style.display = "none";
+}
+function funcion2(){
+    return document.getElementById("sobreMi").style.display = "none";
 }
 
